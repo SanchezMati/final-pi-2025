a47018fe6af2a4d395bc57109f4fc5101b26643c
Mati Sanchez <msancheznovelli@itba.edu.ar>
2025-01-27 17:33:35 -0300

    Cree la primera version del main y un pequenio makefie para testear el main

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
+# Variables
 CC = gcc
 FLAGS = -Wall -pedantic -std=c99 -fsanitize=address -g
 OUTPUT_FILE = test
-OBJ = main.o backend.o
+SOURCE_FILE = main.c
+
+# Regla principal para compilar y ejecutar
+test: $(SOURCE_FILE)
+	$(CC) $(FLAGS) -o $(OUTPUT_FILE) $(SOURCE_FILE)
+
+# Limpieza de archivos generados
+clean:
+	rm -f $(OUTPUT_FILE)
---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define AMOUNT_OF_ARGUMENTS 2
+
+#define TICKETS 1
+#define INFRACTIONS 2
+
+int main(int argc, char* argv[])
+{
+    if(argc != AMOUNT_OF_ARGUMENTS+1)
+    {
+        fprintf(stderr, "Error: Expected %d .csv files, but %d arguments were provided.\n", AMOUNT_OF_ARGUMENTS, argc-1);
+        exit(EXIT_FAILURE);
+    }
+
+
+    //Creo que no hace falta validarlo, (pag 4, enunciado)
+    // for (int i = 1; i <= AMOUNT_OF_ARGUMENTS; i++) 
+    // {
+    //     if (!isCsvFile(argv[i])) 
+    //     {
+    //         fprintf(stderr, "Error: The argument %s is not a .csv file.\n", argv[i]);
+    //         exit(EXIT_FAILURE);
+    //     }
+    // }
+
+    FILE* tickets = fopen(argv[TICKETS], "r");
+    if (tickets == NULL)
+    {
+        fprintf(stderr, "Error al abrir el archivo: %s\n", argv[TICKETS]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE* infractions = fopen(argv[INFRACTIONS], "r");
+    if (infractions == NULL) 
+    {
+        fprintf(stderr, "Error al abrir el archivo: %s\n", argv[INFRACTIONS]);
+        exit(EXIT_FAILURE);
+    }
+
+    // cityADT city = newCity();
+
+    // if(city == NULL)
+    // {
+    //     fprintf(stderr, "Error: Creating CityADT\n");
+    //     fclose(tickets);
+    //     fclose(infractions);
+    //     exit(EXIT_FAILURE);
+    // }
+
+    // freeCity(city);
+
+    puts("OK!");
+    return 0;
+}
