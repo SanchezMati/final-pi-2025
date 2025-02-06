3f2d30e0baeb1b6308ab3a6b60882d245bb13f9b
MatiSanchez <msancheznovelli@itba.edu.ar>
2025-02-06 12:10:17 -0300

    Minor changes

--- backend/src/cityADT.c
+++ backend/src/cityADT.c
@@ -512,7 +512,7 @@ static void getMinMaxAvg(tAgencyDaily * agency, float * min, float * max){
 static void getDateMinMax(tAgencyDaily * agency, char * maxDailyDate, char * minDailyDate){
     int * minDate = agency -> date[MIN];
     int * maxDate = agency -> date[MAX];
-    snprintf(maxDailyDate, MAX_DATE_LENGTH, "%d/%d/%d", maxDate[DD]+1|, maxDate[MM]+1, maxDate[YY]);
+    snprintf(maxDailyDate, MAX_DATE_LENGTH, "%d/%d/%d", maxDate[DD]+1, maxDate[MM]+1, maxDate[YY]);
     snprintf(minDailyDate, MAX_DATE_LENGTH, "%d/%d/%d", minDate[DD]+1, minDate[MM]+1, minDate[YY]);
 }
 
